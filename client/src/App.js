ddd1e074cfb34580c14db94494b9794df9a0c729
OlgaBelogurova <obelogurova2@gmail.com>
2020-11-25 23:44:10 -0800

    Import MessageList to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import MessageList from './components/messages/messageList';
 
 function App() {
   return (
     <div className="App">
-      App
+      <MessageList />
     </div>
   );
 }
