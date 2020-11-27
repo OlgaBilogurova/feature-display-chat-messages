1a30ea9c6ea7f92459d2ff4c873b15ec6b715f08
OlgaBelogurova <obelogurova2@gmail.com>
2020-11-27 14:58:44 -0800

    Convert timestamp to human-readable format

--- src/components/chat/messages/message.js
+++ src/components/chat/messages/message.js
@@ -2,12 +2,23 @@ import React from 'react';
 import './message.css';
 
 const Message = ({ message }) => {
+    const options = {
+        weekday: 'short',
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+        hour: 'numeric',
+        minute: 'numeric'
+    }
+
     return (
         <li className="message-item">
             <div className="message-icon" />
             <div className="message-info">
                 <div className="message-sender">{message.senderUuid},</div>
-                <div className="message-date">{message.sentAt}</div>
+                <div className="message-date">
+                    {new Date(message.sentAt).toLocaleDateString("en-US", options)}
+                </div>
                 <div className="message-content">{message.content}</div>
             </div>
         </li>
